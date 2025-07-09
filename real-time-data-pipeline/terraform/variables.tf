28bf655518afcea3493243682533322c94aef8e9
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-07-09 12:51:34 -0400

    Update variables.tf

--- real-time-data-pipeline/terraform/variables.tf
+++ real-time-data-pipeline/terraform/variables.tf
@@ -1,1 +1,13 @@
-# Placeholder for variables.tf
+# variables.tf
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+  default     = "ancient-cortex-465315-t4"
+}
+
+variable "region" {
+  description = "The GCP region to deploy resources in"
+  type        = string
+  default     = "us-central1"
+}
