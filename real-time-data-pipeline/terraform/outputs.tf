81fb27e4bb550f29549a43476c5c17c13fbfecfb
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-07-09 12:53:28 -0400

    Update outputs.tf

--- real-time-data-pipeline/terraform/outputs.tf
+++ real-time-data-pipeline/terraform/outputs.tf
@@ -1,1 +1,17 @@
-# Placeholder for outputs.tf
+# outputs.tf
+
+output "pubsub_topic" {
+  value = google_pubsub_topic.stream_topic.name
+}
+
+output "storage_bucket" {
+  value = google_storage_bucket.raw_data_bucket.url
+}
+
+output "bigquery_table" {
+  value = google_bigquery_table.processed_table.table_id
+}
+
+output "dataflow_service_account" {
+  value = google_service_account.dataflow_sa.email
+}
