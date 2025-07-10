6291d2319361d958711707eae114378d34cdd192
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-07-10 12:42:42 -0400

    Update variables.tf

--- real-time-data-pipeline/terraform/variables.tf
+++ real-time-data-pipeline/terraform/variables.tf
@@ -11,10 +11,6 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
-variable "image_tag" {
-  description = "Tag for Docker image"
-  type        = string
-  default     = "latest"
-}
+
 
 
