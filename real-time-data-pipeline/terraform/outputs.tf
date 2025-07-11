ea6b1c85d92914395e5e7917055cb9c1b8b7d489
Hemanth349 <Hemanthkumar.hk155@gmail.com>
2025-07-10 20:16:07 -0400

    Update outputs.tf

--- real-time-data-pipeline/terraform/outputs.tf
+++ real-time-data-pipeline/terraform/outputs.tf
@@ -1,5 +1,3 @@
-# outputs.tf
-
 output "pubsub_topic" {
   value = google_pubsub_topic.stream_topic.name
 }
@@ -12,4 +10,9 @@ output "bigquery_table" {
   value = google_bigquery_table.processed_table.table_id
 }
 
-
+/* 
+If the service account resource does not exist currently, comment this output out
+output "dataflow_service_account" {
+  value = google_service_account.dataflow_sa.email
+}
+*/
